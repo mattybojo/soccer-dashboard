f4418cb1ad8ffe922b4963306f2c1ecb05b60ab0
mattybojo <mattbojorquez@gmail.com>
2019-10-02 09:28:40 -0700

    Cleaned up unused imports, changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Estadio Fallon Liga';
+  title: string = 'Estadio Fallon Liga';
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,22 +2,18 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     DashboardRoutingModule,
     CommonModule,
-    MatCardModule,
     MatTableModule,
     MatGridListModule,
-    MatSortModule,
-    MatPaginatorModule
+    MatSortModule
   ]
 })
 export class DashboardModule { }
