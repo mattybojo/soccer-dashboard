4741a40feec18ed2414fdbf6b144664a0c6319b1
mattybojo <mattbojorquez@gmail.com>
2019-10-03 17:26:53 -0700

    Cleaned up MatchService

--- src/app/shared/services/match.service.ts
+++ src/app/shared/services/match.service.ts
@@ -12,24 +12,6 @@ export class MatchService {
 
   constructor(private db: AngularFirestore) {}
 
-  getMatch(dateString: string): Observable<Match[]> {
-    return this.db.
-      collection('matches', ref => ref.where('date', '==', dateString))
-      .snapshotChanges()
-      .pipe(
-        map(snaps => convertSnaps<Match>(snaps))
-      );
-  }
-
-  getLatestMatch(): Observable<Match[]> {
-    return this.db
-      .collection('matches', ref => ref.orderBy('date', 'desc').limit(1))
-      .snapshotChanges()
-      .pipe(
-        map(snaps => convertSnaps<Match>(snaps))
-      );
-  }
-
   getMatches(): Observable<Match[]> {
     return this.db
       .collection('matches', ref => ref.orderBy('date', 'desc'))
